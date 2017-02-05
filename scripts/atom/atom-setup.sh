b440377a9185e11b9d25f4915c0579dabf465bea
Tommy Le Blanc <tlcommodore@gmail.com>
2017-02-05 09:37:48 -0600

    Update atom setup files

--- scripts/atom/atom-setup.sh
+++ scripts/atom/atom-setup.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env bash
-#Filename: atom-setup.sh
+# Filename: atom-setup.sh
 
-echo
-echo "-------------- Atom editor packages --------------"
+# TODO: Include the ability to download and self-install Atom
 
 # Install commonly used packages given a text file with said packages
 install_atom_packages () {
-  echo "  Downloading/Installing atom packages..."
+  echo "@--> Downloading/Installing atom packages..."
 
-  apm install --packages-file atom-packages.txt
+  apm install --packages-file scripts/atom/atom-packages.txt
   return
 }
 
 # Main
+echo
+echo "@> -------------- Atom editor packages --------------"
+echo "@> Setting up Atom packages (and soon settings)..."
+
 install_atom_packages
+
+echo "@> Done!"
