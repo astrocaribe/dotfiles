1b3717d799abd250f258d5758b3e84d321ece70e
Tommy LeBlanc <tlcommodore@gmail.com>
2017-02-07 11:28:55 -0600

    Add git alias for uncommiting (last commit)

--- scripts/git/git-setup.sh
+++ scripts/git/git-setup.sh
@@ -15,6 +15,7 @@ function set_aliases () {
 	git config --global alias.ci commit
 	git config --global alias.st status
 	git config --global alias.unstage 'reset HEAD --'
+	git config --global alias.uncommit 'reset --soft HEAD^'
 	git config --global alias.last 'log -1 HEAD'
 	git config --global alias.visual '!gitk'
 
