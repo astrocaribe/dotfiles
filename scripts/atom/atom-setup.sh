e90cd950230e6cca6c620afc23a23ae6112e37fd
Tommy LeBlanc <tlcommodore@gmail.com>
2017-02-11 10:37:57 -0600

    Improve Atom editor setup
      - Download, build, and install latest Atom binary
      - Include symbolic links for atom, apm to aid package install step
      - Move main execution into separate *main* function
      - Use $PWD as root directory instead of absolute
      - Add an uninstall script to help with testing
      - Include personalized Atom configs to be copied over to .atom dir

--- scripts/atom/atom-setup.sh
+++ scripts/atom/atom-setup.sh
@@ -1,21 +1,70 @@
 #!/usr/bin/env bash
 # Filename: atom-setup.sh
 
-# TODO: Include the ability to download and self-install Atom
+### Description: Installs Atom editor from source (GitHub), installs packages,
+### then copies configurations from configs folder.
+### Author: T.LeBlanc, tlcommodore@gmail.com 02.2017
 
-# Install commonly used packages given a text file with said packages
-install_atom_packages () {
-  echo "@--> Downloading/Installing atom packages..."
+root_dir=$PWD
 
-  apm install --packages-file scripts/atom/atom-packages.txt
+# Install Atom Editor
+function install_atom () {
+  echo "@--> Downloading/Installing atom ..."
+
+  mkdir $root_dir/temp; cd $root_dir/temp
+  git clone https://github.com/atom/atom.git
+  cd atom
+  script/build --install
+
+  # Create atom and apm symbolic links
+  ln -s /Applications/Atom.app/Contents/Resources/app/apm/node_modules/.bin/apm /usr/local/bin
+  ln -s /Applications/Atom.app/Contents/Resources/app/atom.sh /usr/local/bin
+
+  # Return to root
+  cd $root_dir
+
   return
 }
 
-# Main
-echo
-echo "@> -------------- Atom editor packages --------------"
-echo "@> Setting up Atom packages (and soon settings)..."
+# Install commonly used packages given a text file with said packages
+function install_atom_packages () {
+  echo "@--> Downloading/Installing atom packages ..."
 
-install_atom_packages
+  apm install --packages-file $root_dir/atom-packages.txt
+  return
+}
 
-echo "@> Done!"
+# Copies configurations for installed packages
+function copy_configs () {
+  echo "@--> Copying configs ..."
+  cp $root_dir/configs/* ~/.atom/
+
+  return
+}
+
+# Cleanup
+function cleanup () {
+  echo "@--> Removing temp folder..."
+
+  rm -rf temp
+
+  return
+}
+
+function main () {
+  echo
+  echo "@> -------------- Atom editor packages --------------"
+  echo "@> Setting up Atom editor ..."
+
+  install_atom                # Download latest atom, build, and install
+  install_atom_packages       # Install user packages from atom-packages.txt
+  copy_configs                # Copy user config files to atom
+  cleanup                     # Remove temp folder
+
+  echo "@> Done!"
+
+  return
+}
+
+# Main function
+main "$@"
